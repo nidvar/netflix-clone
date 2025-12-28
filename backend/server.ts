2948050a3c26141074f41ffa14ffdb9af23306d7
nidvar <djayshine87@hotmail.com>
2025-12-29 10:49:10 +1100

    backend connected

--- backend/server.ts
+++ backend/server.ts
@@ -4,11 +4,21 @@ dotenv.config();
 import express from "express";
 
 import router from "./routes/auth.route.js";
+import pool from "./db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.get("/health", async (req, res) => {
+  try {
+    console.log("Checking database connection...");
+    const result = await pool.query("SELECT NOW()");
+    res.json({ dbTime: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
 
 app.use("/api/auth", router);
 
