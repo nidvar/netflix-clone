d2379613667f226be78a5cb0f54605e655c296a0
nidvar <djayshine87@hotmail.com>
2025-12-30 09:46:15 +1100

    refactor auth controllers

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -5,33 +5,54 @@ import type { Request, Response } from 'express';
 import pool from '../db.js';
 import { generateAccessToken, generateRefreshToken } from '../utils/generateToken.js';
 
+const validateCredentials = (email: string, password: string) => {
+    if (
+        !email || 
+        !password || 
+        email.trim() === '' || 
+        password.trim() === ''
+    ) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
 export const login = async (req: Request, res: Response)=>{
     try {
-        if (
-            !req.body.email || 
-            !req.body.password || 
-            req.body.email.trim() === '' || 
-            req.body.password.trim() === ''
-        ) {
+
+        if(validateCredentials(req.body.email, req.body.password) === false){
             return res.status(400).json({ message: 'Email and password error' });
         };
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
-        if(result.rows[0]?.email === req.body.email){
-            if(req.body.password === result.rows[0]?.password){
 
-                const accessToken = generateAccessToken({ id: result.rows[0].id });
+        const result = await pool.query(
+            'SELECT * FROM users WHERE email = $1', 
+            [req.body.email]
+        );
 
-                res.cookie('accessToken-netflix-clone', accessToken, {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV !== 'development',
-                    sameSite: 'strict',
-                    maxAge: 15 * 60 * 1000,
-                });
+        const user = result.rows[0];
 
-                return res.status(200).json({ message: 'Login successful' });
-            };
+        if(!user){
+            return res.status(401).json({ message: 'Invalid credentials' });
         };
-        return res.status(401).json({ message: 'Invalid credentials' });
+
+        const passwordValidate = await bcrypt.compare(req.body.password, user.password);
+
+        if(!passwordValidate){
+            return res.status(401).json({ message: 'Invalid credentials' });
+        };
+
+        const accessToken = generateAccessToken({ id: user.id });
+
+        res.cookie('accessToken-netflix-clone', accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 15 * 60 * 1000,
+        });
+
+        return res.status(200).json({ message: 'Login successful' });
+
     } catch (error) {
         console.error('Error in login controller:', error);
         res.status(500).json({ message: 'Internal server error' });
@@ -40,23 +61,23 @@ export const login = async (req: Request, res: Response)=>{
 
 export const signUp = async (req: Request, res: Response)=>{
     try {
-        if (
-            !req.body.email || 
-            !req.body.password || 
-            req.body.email.trim() === '' || 
-            req.body.password.trim() === ''
-        ) {
+
+        if(validateCredentials(req.body.email, req.body.password) === false){
             return res.status(400).json({ message: 'Email and password error' });
         };
-        const existingUser = await pool.query(
+
+        const result = await pool.query(
             'SELECT * FROM users WHERE email = $1',
             [req.body.email]
         );
-        if(existingUser.rowCount && existingUser.rowCount > 0) {
-            return res.status(409).json({ message: 'User already exists' });
-        };
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
+        const existingUser = result.rows[0];
+
+        if(existingUser) {
+            return res.status(500).json({ message: 'User already exists' });
+        };
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const image = req.body.image || 'avatar.png';
 
         await pool.query(
