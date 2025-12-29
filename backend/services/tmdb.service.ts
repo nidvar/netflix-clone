dbd571a6a05fdcf1493f56a2f0ac55531833314c
nidvar <djayshine87@hotmail.com>
2025-12-30 10:53:01 +1100

    tmdb api added

---
+++ backend/services/tmdb.service.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const fetchData = async (customURL?: string) => {
+    let url = customURL || 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer ' + process.env.API_Read_Access_Token
+        }
+    };
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching popular movies:', error);
+        throw error;
+    }
+}
