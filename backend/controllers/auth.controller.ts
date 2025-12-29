36050ec46f046c11e8d6fe061160445069a0be49
nidvar <djayshine87@hotmail.com>
2025-12-29 17:17:42 +1100

    generate tokens and cookies

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import type { Request, Response } from 'express';
 
 import pool from '../db.js';
+import { generateAccessToken, generateRefreshToken } from '../utils/generateToken.js';
 
 export const login = async (req: Request, res: Response)=>{
     try {
@@ -17,6 +18,16 @@ export const login = async (req: Request, res: Response)=>{
         const result = await pool.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
         if(result.rows[0]?.email === req.body.email){
             if(req.body.password === result.rows[0]?.password){
+
+                const accessToken = generateAccessToken({ id: result.rows[0].id });
+
+                res.cookie('accessToken-netflix-clone', accessToken, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV !== 'development',
+                    sameSite: 'strict',
+                    maxAge: 15 * 60 * 1000,
+                });
+
                 return res.status(200).json({ message: 'Login successful' });
             };
         };
@@ -45,9 +56,14 @@ export const signUp = async (req: Request, res: Response)=>{
             return res.status(409).json({ message: 'User already exists' });
         };
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const image = req.body.image || 'avatar.png';
+
         await pool.query(
-            'INSERT INTO users (email, password) VALUES ($1, $2)', [req.body.email, hashedPassword]
+            'INSERT INTO users (email, password, image) VALUES ($1, $2, $3)', 
+            [req.body.email, hashedPassword, image]
         );
+
         return res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error in signUp controller:', error);
---
+++ backend/utils/generateToken.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import jwt from 'jsonwebtoken';
+
+export const generateAccessToken = (payload: {id: string}) => {
+    return jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '15m' });
+};
+
+export const generateRefreshToken = (payload: {id: string}) => {
+    return jwt.sign(payload, process.env.REFRESH_JWT_SECRET as string, { expiresIn: '1h' });
+}
