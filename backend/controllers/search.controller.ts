f02bcbbbfa3cbf12ad164b1377de07588c2ce923
nidvar <djayshine87@hotmail.com>
2026-01-07 09:40:13 +1100

    search people and tv shows added

--- backend/controllers/search.controller.ts
+++ backend/controllers/search.controller.ts
@@ -1,12 +1,36 @@
 import type { Request, Response } from 'express';
 import { fetchData } from '../services/tmdb.service.js';
 
-export const searchMovie = async (req: Request, res: Response)=>{
+export const searchPerson = async (req: Request, res: Response)=>{
     const query = req.params.query;
     try {
         const data = await fetchData(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        const people = data.results;
+        return res.status(200).json({people: people});
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: 'search movie error'});
+    }
+}
+
+export const searchMovie = async (req: Request, res: Response)=>{
+    const query = req.params.query;
+    try {
+        const data = await fetchData(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
         const movies = data.results;
         return res.status(200).json({movies: movies});
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: 'search movie error'});
+    }
+}
+
+export const searchTVShow = async (req: Request, res: Response)=>{
+    const query = req.params.query;
+    try {
+        const data = await fetchData(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`);
+        const tvshows = data.results;
+        return res.status(200).json({tvshows: tvshows});
     } catch (error) {
         console.log(error);
         return res.status(401).json({message: 'search movie error'});
--- backend/routes/search.route.ts
+++ backend/routes/search.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { searchMovie } from '../controllers/search.controller.js';
+import { searchMovie, searchPerson, searchTVShow } from '../controllers/search.controller.js';
 
 const searchRouter = express.Router();
 
 searchRouter.get('/movies/:query', searchMovie);
+searchRouter.get('/tv/:query', searchTVShow);
+searchRouter.get('/person/:query', searchPerson);
 
 export default searchRouter;
