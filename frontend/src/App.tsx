e005124a16e6a47ffd92c87bc7aa05881949d2a0
nidvar <djayshine87@hotmail.com>
2026-01-07 18:37:46 +1100

    frontend boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>NETFLIX CLONE</h1>
+    </>
+  )
+}
+
+export default App
