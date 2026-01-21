52da899d8114ced2d34477d241bf543e52c1a263
nidvar <djayshine87@hotmail.com>
2026-01-21 11:51:38 +1100

    refresh and access token timers extended

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -4,9 +4,9 @@ dotenv.config();
 import jwt from 'jsonwebtoken';
 
 export const generateAccessToken = (payload: {id: string}) => {
-    return jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '2m' });
+    return jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '30m' });
 };
 
 export const generateRefreshToken = (payload: {id: string}) => {
-    return jwt.sign(payload, process.env.REFRESH_JWT_SECRET as string, { expiresIn: '1h' });
+    return jwt.sign(payload, process.env.REFRESH_JWT_SECRET as string, { expiresIn: '3h' });
 };
