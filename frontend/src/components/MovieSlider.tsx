b46ad1cbd9454990240b7044a9657ceaca46b247
nidvar <djayshine87@hotmail.com>
2026-01-25 12:48:19 +1100

    movieslider componenent now has converMovie/People Data functions. the jsx now returns unified data object to include people

--- frontend/src/components/MovieSlider.tsx
+++ frontend/src/components/MovieSlider.tsx
@@ -5,14 +5,16 @@ import { ChevronRight, ChevronLeft  } from "lucide-react";
 
 import { useContentTypeStore } from "../store/contentTypeStore";
 
-import type { MovieSliderProps, MovieType } from "../types";
+import type { MovieSliderProps, DataObjectType, MovieType, PeopleType } from "../types";
 import { fetchRequest } from "../utils/functions";
+import { useSearchResultsStore } from "../store/searchResultsStore";
 
 function MovieSlider(props: MovieSliderProps) {
 
   const contentTypeStore = useContentTypeStore();
+  const searchStore = useSearchResultsStore();
 
-  const [data, setData] = useState<MovieType[]>([]);
+  const [data, setData] = useState<DataObjectType[]>([]);
   const [showArrows, setShowArrows] = useState(false);
 
   const navigate = useNavigate();
@@ -33,9 +35,41 @@ function MovieSlider(props: MovieSliderProps) {
   const grabData = async ()=>{
     const data = await fetchRequest('/movies/' +  contentTypeStore.contentType + '/categories/' + props.category);
     if(data.movies.results){
-      setData(data.movies.results);
-    }
+      convertMovieData(data.movies.results);
+    };
   };
+
+  const convertMovieData = function(arr: MovieType[]){
+    let newArr: DataObjectType[] = [];
+    arr.forEach((item: MovieType)=>{
+      if(item.backdrop_path == null && item.poster_path == null){
+          return
+        };
+        newArr.push({
+          backdropURL: item.backdrop_path || item.poster_path,
+          posterURL: item.poster_path,
+          title: item.title || item.original_title || item.name || item.original_name || '',
+          id: item.id,
+        });
+    });
+    setData(newArr);
+  }
+
+  const convertPeopleData = function(arr: PeopleType[]){
+    let newArr: DataObjectType[] = [];
+    arr.forEach((item: PeopleType)=>{
+      if(item.profile_path == null){
+          return
+        };
+        newArr.push({
+          backdropURL: item.profile_path,
+          posterURL: item.profile_path,
+          title: item.name,
+          id: item.id,
+        });
+    });
+    setData(newArr);
+  }
 
   const scrollLeft = () => {
     if (sliderRef.current) {
@@ -58,12 +92,21 @@ function MovieSlider(props: MovieSliderProps) {
   useEffect(()=>{
     if(!props.ownData){
       grabData();
+    }else{
+      console.log('we are using our own data')
     }
   }, [contentTypeStore.contentType]);
 
+  
   useEffect(()=>{
-    if(props.data && props.data.length > 0){
-      setData(props.data);
+    if(props.peopleData){
+      convertPeopleData(props.peopleData);
+    }
+  }, [props.peopleData]);
+
+  useEffect(()=>{
+    if(props.data){
+      convertMovieData(props.data);
     }
   }, [props.data]);
 
@@ -91,7 +134,7 @@ function MovieSlider(props: MovieSliderProps) {
         {
           data.length > 0?
           data.map((item)=>{
-            if(item.backdrop_path === null && item.poster_path === null){
+            if(item.backdropURL === null && item.posterURL === null){
               return;
             }
             return (
@@ -105,11 +148,11 @@ function MovieSlider(props: MovieSliderProps) {
               >
                 <img
                   alt="Movie image"
-                  src={props.ownData? imageSrc + item.poster_path:  imageSrc + item.backdrop_path}
+                  src={props.ownData? imageSrc + item.posterURL:  imageSrc + item.backdropURL}
                   className={"transition-transform duration-300 ease-in-out group-hover:scale-105"}
                   onClick={function(){navigate("/watch/" + item.id)}}
                 />
-                <p className="center">{item.title? item.title: item.name}</p>
+                <p className="center">{item.title}</p>
               </div>
             )
           }):''
