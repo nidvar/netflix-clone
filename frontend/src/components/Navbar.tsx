a8f2a9f932f8fa8ec219afa82b33973191b3e94c
nidvar <djayshine87@hotmail.com>
2026-01-25 16:16:35 +1100

    input value cleared when toggling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -81,6 +81,7 @@ function Navbar() {
   const closeSearch = function(e: Event){
     if(searchRef.current && !searchRef.current.contains(e.target as Node)){
       setShowSearch(false);
+      setInputValue('');
     }
   }
 
