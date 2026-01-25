102ee2b3a5b47ca2300e731f37071d0fd0fc3ce4
nidvar <djayshine87@hotmail.com>
2026-01-25 13:46:24 +1100

    search results text removed for now

--- frontend/src/pages/SearchResultsPage.tsx
+++ frontend/src/pages/SearchResultsPage.tsx
@@ -1,38 +1,31 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 import { useSearchResultsStore } from '../store/searchResultsStore';
 
-function SearchResults() {
-  const navigate = useNavigate();
+import MovieSlider from '../components/MovieSlider';
 
+function SearchResults() {
+
+  const navigate = useNavigate();
   const searchStore = useSearchResultsStore();
 
-  const [results, setResults] = useState('');
-
   useEffect(()=>{
-    console.log('searching: ', searchStore.searching);
-    if(
-      searchStore.movies.length === 0 || 
-      searchStore.tvshows.length === 0 || 
-      searchStore.people.length === 0
-    ){
-      setResults('No results found');
-    }
-  }, [searchStore.searching]);
+    if(searchStore.searchValue == ''){
+      navigate('/');
+    };
+  }, []);
 
   return (
-    <div className='white'>
-      <h1>Search Results</h1>
-      <p>{searchStore.searching? "Searching..." : results}</p>
-
-      <div className='search-results-container flex flex-wrap'>
-        {
-          searchStore.searching? 
-          'searching....' : ''
-        }
+    <div className="bg-black white pt-1">
+      <div className='search-page-container min-h-screen'>
+        <h1 className='center'>Search Results</h1>
+        <div className="flex flex-col gap-10 p-10 bg-black">
+          <MovieSlider category='Movies' ownData={true} data={searchStore.movies} />
+          <MovieSlider category='TV Shows' ownData={true} data={searchStore.tvshows} />
+          <MovieSlider category='People' ownData={true} peopleData={searchStore.people} />
+        </div>
       </div>
-      
     </div>
   )
 }
